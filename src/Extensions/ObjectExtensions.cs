20e6a4b04e314dce36d5b78c967b95d1f1d4ef7b
mark.kockerbeck <xebxeb@gmail.com>
2010-03-19 16:28:02 -0700

    :q

---
+++ src/Extensions/ObjectExtensions.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Linq;
+
+namespace Just.Core.Extensions
+{
+	public static class ObjectExtensions
+	{
+		/// <summary>
+		/// Will perform the given <see cref="Func{T}" /> on the instance if instance is not null
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <typeparam name="TK"></typeparam>
+		/// <param name="instance"></param>
+		/// <param name="someAction"></param>
+		/// <returns></returns>
+		public static T SafeCall<T, TK>(this TK instance, Func<TK,T> someAction) where TK : class
+		{
+			return SafeCall(instance, someAction, default(T));
+		}
+
+		/// <summary>
+		/// Will perform the given <see cref="Func{T}" /> on the instance if instance is not null.  Returns <see cref="T"/> defaultValue if instance is NULL.
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <typeparam name="TK"></typeparam>
+		/// <param name="instance"></param>
+		/// <param name="someAction"></param>
+		/// <param name="defaultValue"></param>
+		/// <returns></returns>
+		public static T SafeCall<T,TK>(this TK instance, Func<TK,T> someAction, T defaultValue) where TK : class
+		{
+			return instance == null ? defaultValue : someAction(instance);
+		}
+
+		/// <summary>
+		/// Parses the given value into <see cref="Type"/> of type
+		/// </summary>
+		/// <param name="value"></param>
+		/// <param name="type"></param>
+		/// <returns></returns>
+		public static object ParseValue(this object value, Type type)
+		{
+			try
+			{
+				if (type.GetNonNullableType().IsEnum)
+				{
+					return Enum.Parse(type.GetNonNullableType(), Convert.ToString(value));
+				}
+
+				if (type.IsEnum)
+				{
+					return Enum.Parse(type, Convert.ToString(value));
+				}
+
+				if(type.GetNonNullableType() != type)
+				{
+					if (value == null || String.IsNullOrEmpty(Convert.ToString(value)))
+					{
+						return null;
+					}
+
+					return Convert.ChangeType(value, type.GetNonNullableType());
+				}
+
+				return Convert.ChangeType(value, type);
+			}
+				// ReSharper disable EmptyGeneralCatchClause
+			catch
+				// ReSharper restore EmptyGeneralCatchClause
+			{
+
+			}
+
+			return value;
+		}
+
+		/// <summary>
+		/// Will attempt to parse the value of an object to T
+		/// </summary>
+		/// <example>
+		/// bool debug = ConfigurationManager.AppSetting["debug"].ParseValue(true);
+		/// int timeout = Request.QueryString["timeout"].ParseValue(60);
+		/// DateRange? selection = Session["selection"].ParseValue(DateRange.Days);
+		/// </example>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="value"></param>
+		/// <param name="defaultValue"></param>
+		/// <returns></returns>
+		public static T ParseValue<T>(this object value, T defaultValue)
+		{
+			try
+			{
+				if (typeof(T).GetNonNullableType().IsEnum)
+				{
+					return (T)Enum.Parse(typeof(T).GetNonNullableType(), Convert.ToString(value));
+				}
+
+				if (typeof(T).IsEnum)
+				{
+					return (T)Enum.Parse(typeof(T), Convert.ToString(value));
+				}
+
+				return (T)Convert.ChangeType(value, typeof(T));
+			}
+				// ReSharper disable EmptyGeneralCatchClause
+			catch
+				// ReSharper restore EmptyGeneralCatchClause
+			{
+
+			}
+
+			return defaultValue;
+		}
+	}
+}
---
+++ src/Extensions/TypeExtensions.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Just.Core.Extensions
+{
+	public static class TypeExtensions
+	{
+		/// <summary>
+		/// Get's the base type of the Nullable
+		/// </summary>
+		/// <param name="type"></param>
+		/// <returns></returns>
+		public static Type GetNonNullableType(this Type type)
+		{
+			if (type != null)
+			{
+				if (type.FullName.StartsWith("System.Nullable`1[["))
+				{
+					string typeName = type.FullName.Replace("System.Nullable`1[[", "").Replace("]]", "");
+					return Type.GetType(typeName);
+				}
+			}
+
+			return type;
+		}
+
+		/// <summary>
+		/// Returns a list of the fields for a type -- particularly useful for enums
+		/// </summary>
+		/// <example>
+		/// typeof(Enum).ToFieldList()
+		/// </example>
+		/// <param name="type"></param>
+		/// <returns></returns>
+		public static List<String> ToFieldList(this Type type)
+		{
+			var list = new List<String>();
+
+			foreach(var field in type.GetFields())
+			{
+				list.Add(field.Name);
+			}
+
+			return list;
+		}
+
+		/// <summary>
+		/// Determines if the type implements the interfaceType
+		/// </summary>
+		/// <param name="type"></param>
+		/// <param name="interfaceType"></param>
+		/// <see cref="http://www.hanselman.com/blog/DoesATypeImplementAnInterface.aspx"/>
+		/// <returns></returns>
+		public static Boolean IsImplementationOf(this Type type, Type interfaceType)
+		{
+			return type.GetInterface(interfaceType.FullName) != null;
+		}
+	}
+}
